b23fea8d4c696f151b2769561d4450dcb4132eff
Efim <efimpost@gmail.com>
2022-01-03 15:40:03 +0300

    Добавлены коменты.

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from telebot import types
 import random
 import xlrd
 
-bot = telebot.TeleBot('token')
+bot = telebot.TeleBot('TOKEN.txt')             # Создание экземпляра бота и подключение токена
 
-@bot.message_handlers(commands=['start'])
+@bot.message_handlers(commands=['start'])      # Создание стартового сообщения
 def start_messege(messege):
-    bot.send_message(messege.chat.id, 'Приветствую странник! Ты вступил на путь шиншиллы.')
+    bot.send_message(messege.chat.id, 'Приветствую странник! Ты вступил на путь шиншиллы.')  # Само сообщение
+    picture = open('chichella.webp', 'rb')     # Создание переменной с каратинкой
+    bot.send_sticker(messege.chat.id, picture) # показать картинку
 
 
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0)       # Опрос сервера, не написал ли кто-нибудь?
