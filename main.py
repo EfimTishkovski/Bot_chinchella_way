4222122d6d4f664cfde6ff0dc3cc30430635f772
efim <schmurge22@mail.ru>
2022-01-04 21:15:13 +0300

    Добавлено знание шиншиллы.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import datetime
-
 import telebot
 from telebot import types
-import time
 import random
-#import xlrd
 
 # Чтение токена из фийла
 tok = open('TOKEN.txt', 'r')
 TOKEN = tok.read()
 
 bot = telebot.TeleBot(TOKEN)             # Создание экземпляра бота и подключение токена
-
+# Источник мудрости Шинниллы
+citate_mass = ['Я скала! Я кремень!',
+               'Муррррр...©Кот',
+               'Чужая душа - потёмки, но ингда нужно сделать этот шаг в темноту.',
+               'Немогу - значит не могу, не можешь и не надо, остановись, поверни назад и останешся жив. ©Анатолий Букреев.',
+               'Истории о деньгах и успехе не удивительны, история о том что кто-то по настоящему любил, по настоящему дружил, удивительна',
+]
 print('Запущено')
 
 @bot.message_handler(commands=['start'])      # Создание стартового сообщения
@@ -24,9 +27,17 @@ def start_messege(messege):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn_source = types.KeyboardButton('Источник')                    # Создание кнопки
     btn_citation = types.KeyboardButton('Цитата')                    # Создание кнопки
-    btn_message_to_source = types.KeyboardButton('Послание Шиншилле') # Создание кнопки
-    markup.add(btn_source, btn_citation, btn_message_to_source)      # Добавление кнопок
+    markup.add(btn_source, btn_citation)                             # Добавление кнопок
     bot.reply_to(messege, 'Чтобы поговорить с шиншилой, жми на кнопки ниже', reply_markup=markup)
+
+# Функция работы меню помощи(/help)
+@bot.message_handler(commands=['help'])
+def help_post(message):
+    bot.send_message(message.chat.id,   '/start - Запустить бота заново\n' +
+                                        '/help - Запустить меню справки\n' +
+                                        'Источник - Узнать откуда всё началось\n' +
+                                        'Цитата - Получить рандомную цитату из запасов шиншиллы\n' +
+                                        '"Послание" - Написать в сообщении и шиншила примет и запомнит ваше послание\n')
 
 # Функция обработки нажатий на кнопки
 @bot.message_handler(func=lambda message: True)
@@ -41,9 +52,8 @@ def menu(message):
             bot.send_message(message.chat.id, 'Вначале, это был путь страданий, но потом он стал путём силы.')
             bot.send_message(message.chat.id, 'Девиз: Я скала, я кремень!')
         elif message.text == 'Цитата':
-            bot.send_message(message.chat.id, 'Тут будет цитата из списка.')
-        elif message.text == 'Послание Шиншилле':
-            bot.send_message(message.chat.id, 'Неопределённа кнопка, может не понадобится.')
+            #bot.send_message(message.chat.id, 'Тут будет цитата из списка.')
+            bot.send_message(message.chat.id, random.choice(citate_mass))
         elif message.text == 'Послание':
             messege_to_chinchella(message)
         else:
