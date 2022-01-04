e49c4f7c85cbccc405b1e135ccd49f54633058bb
Efim <efimpost@gmail.com>
2022-01-04 10:06:02 +0300

    Добавлены кнопки клавиатуры.

--- main.py
+++ main.py
@@ -17,5 +17,12 @@ def start_messege(messege):
     picture = open('chichella.webp', 'rb')     # Создание переменной с каратинкой
     bot.send_sticker(messege.chat.id, picture) # показать картинку
 
+    # Клавиатура (кнопки)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_source = types.KeyboardButton('Источник')                    # Создание кнопки
+    btn_citation = types.KeyboardButton('Цитата')                    # Создание кнопки
+    btn_message_to_source = types.KeyboardButton('Послание Шиншиле') # Создание кнопки
+    markup.add(btn_source, btn_citation, btn_message_to_source)      # Добавление кнопок
+    bot.reply_to(messege, 'Чтобы поговорить с шиншилой, жми на кнопки ниже', reply_markup=markup)
 
 bot.polling(none_stop=True, interval=0)       # Опрос сервера, не написал ли кто-нибудь?
